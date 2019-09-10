cdbd4fa358757c400889c92c8b383b615c084820
Hugo Meireles <meireles.hmm@gmail.com>
2019-09-10 22:32:10 +0100

    Insert more configuration to the server

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get('/me', (req, res) => res.send('API Running...'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 const PORT = process.env.PORT || 5000;
 
